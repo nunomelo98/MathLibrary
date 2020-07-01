f3e5b176fb5baf4aa5db548e279f76f4441b219a
Nuno.melo <nuno.melo@nosi.cv>
2020-07-01 16:38:17 -0100

    first Commit

---
+++ app/src/main/java/com/nunomelo/mathlibrary/Operacoes.kt
@@ -0,0 +1,31 @@
+package com.exemplo.mathlibrary
+
+
+object Operacoes {
+
+    fun Plus(  a : Int,  b : Int) : Int{
+
+        return a + b
+    }
+
+    fun Minus(  a : Int,  b : Int) : Int{
+
+        return a - b
+    }
+
+    fun Times(  a : Int,  b : Int) : Int{
+
+        return a * b
+    }
+
+    fun Dev(  a : Int,  b : Int) : Float {
+
+        return if (b == 0) {
+            throw IllegalArgumentException("Div by Zero Error!")
+        } else {
+            (a / b).toFloat()
+
+        }
+    }
+
+}
